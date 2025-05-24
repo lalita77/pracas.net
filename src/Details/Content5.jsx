4d0a38307a374425d78198e54eda31351ff93834
lalita77 <lalitaghimire330@gmail.com>
2025-05-24 23:02:15 +0545

    Completed 5th content.

--- src/Details/Content5.jsx
+++ src/Details/Content5.jsx
@@ -9,8 +9,64 @@ function Content5() {
           <div className="intro">
             <img className='rounded-2xl w-full max-h-[450px] lg:h-[450px] my-5 object-cover' src="/image/content5-image/1.png" alt="" />
             <h4 className='text-[18px] Text-[#4b4b4b] mb-5 font-bold'>The unmanaged website</h4>
-           <p> <i>By: Pracas Upreti, Information Technology Consultant<br />
+            <p> <i>By: Pracas Upreti, Information Technology Consultant<br />
               Published on: July 20, 2021</i>
+            </p>
+            <p className='text-justify my-5'>
+              During my college days at Hyderabad (2000-2006). I was keen interested to make a personal website, this was a great dream for someone in 2003. As my personal email was unable to get registered as prakash@hotmail.com, I changed k to c and made email on hotmail <strong>as pracas@hotmail.com during 2000 which had space of only 2MB</strong> and it became my digital initial name. And after researching domain name system in early days in Google, I finally wanted to make domain name as www.pracas.com (captured by Sedo Parking since 2005) and <u>Net4 India</u> Hyderabad was then an early domain registration company which charged me nearly INR 600 to get the domain registered and my days started as a website enthusiast.
+
+              <img className='rounded-xl w-full max-h-[450px] lg:h-[450px] my-5' src="/image/content5-image/2.png" alt="" /> </p>
+
+            <p className='text-justify my-5'>
+              Being an IT Student, most of my days and nights were invested in Computer of a senior who was gifted a Desktop Computer from him elder brother, who then was working in <a className='text-blue-700 hover:underline font-semibold' href="Lockheed Martin">Lockheed Martin</a> in the USA. <br /> <br />
+
+              Those days internet was luxury, only few homes had access with dialup and somewhere DSL Connection those days. Somehow I managed to learn website designing in Photoshop 7 with Dreamweaver MX 2004. I had a sister who was had gifted me "Dreamweaver MX 2004 Bible" written by <a className='text-blue-700 hover:underline font-semibold' href="Joseph Lowery">Joseph Lowery</a>, instead of "T-Shirt" upon my request and I read that book nearly 13 times to know all hidden corners of Dreamweaver Bible.
+
+              <img className='rounded-xl w-full max-h-[450px] lg:h-[450px] my-5' src="/image/content5-image/3.jpeg" alt="" /> </p>
+
+            <p className='text-justify my-5'>
+              I started my Tech Career on 2003 as a student and till now, I feel proud calling myself as an IT Student as learning never ends and the thirst will never fulfill. Reading one of my senior's book, I came to know about about FTP and those days <a href="">Cute FTP</a> was the best ftp client to transfer files from local computer to remote server or vice versa. <br /> <br />
+
+              While traveling in the train from <a href="">Puttaparthi</a> to Bangalore, I had met a co-passenger who was from <a href="">Kathmandu</a> and he guided me about free web hosting website which at those time let users to host their website freely by keeping their advertisement in header and footer. He is my inspiration too. Crossing these nearly two decades of journey, I have been inspired with various people from various countries who guided, inspired and supported me in every aspects of my personal and tech life. <br /> <br />
+
+              The process of web design in early days were to design website in <a href="">Photoshop 7</a> slice to load images and to manage codes in Dreamweaver which was best those days and not much hand coding were needed. As we everyone know, time changes and upgrades most of the things and Information Technology is majorly affected by this change. Upgrade of OS, patch, updates etc makes computer slower and upgrade of even small MS Office files are larger these days but they are easy to use the latest trend and vast features to drag and drop and manage many things i.e. saving automatically in cloud, drive synchronization.
+
+              <img className='rounded-xl w-full max-h-[450px] lg:h-[450px] my-5' src="/image/content5-image/4.gif" alt="" /> </p>
+
+            <p className='text-justify my-5'>
+              My tech journey provided me to register 1000+ domain names and I have uploaded almost 500+ websites till now which was part of my personal work, professional and also outsourcing to USA, Canada, UAE, Australia, Ghana, South Africa, Qatar, India and few other countries. But due to adaption of Smartphones people started moving from PC to laptops and from then people started adapting handheld devices some small and some very large, connecting people with devices.
+              <br /> <br />
+
+              Seeing these web development trends from 2015 to 2021, an organization invests a lot IT Infrastructure, manpower, furniture, workforce and decoration but they have very less budget to showcase their product, a restaurant owner started home delivery business but instead of investing in digital technology like website and app, he focuses in decoration of his restaurant and this is not the fault of the owner, the fault it the mindset. When people feel sick they consult doctor, but when they loose business they are clueless. Here in Nepal there are not enough organisation or services which provides Business Consulting services and somewhere it is very hard to make people understand to manage and audit their IT infrastructure, their presence in social media and also their vision and plans to enhance their business through Information Technology. <br /> <br />
+
+              Being in Kathmandu for few days, while browsing website of few <strong>Ministries of Nepal Government</strong> and also corporates, I saw their website was not managed properly which not only lacks the information but also it creates negative impact towards the country and people. A sample website has been featured below which lacks proper image ratio, no SSL certificate and few more things.
+
+              <img className='rounded-xl w-full max-h-[450px] lg:h-[450px] my-5' src="/image/content5-image/5.png" alt="" />
+
+              Creating website is art and logic. When both of the things are blended it will present the best things. Few things are essential in website these days which are as follows :
+
+              <ul className="list list-disc ps-4 my-5">
+                <li>OG Protocol</li>
+                <li>Call to Action Button </li>
+                <li>Favicon</li>
+                <li>SSL Certificate</li>
+                <li>Live Chat System</li>
+                <li>Proper use of Title and Meta Description</li>
+                <li>Avoiding cursive writing fonts</li>
+                <li>Search Engine Friendly Tags</li>
+                <li>Contact Forms</li>
+                <li>Coding according to Google Rank Brain system</li>
+                <li>Avoiding large images for small placement</li>
+                <li>Social Media Integration</li>
+                <li>Images cropped in aspect ratio</li>
+                <li>Using proper CSS tags</li>
+                <li>Hosting Location & access load</li>
+                <li>Bandwidth</li>
+                <li>Website Speed etc</li>
+              </ul>
+
+              Managing few of the above things can make good impact to viewers and every developer or designer should build website from Surfer's View. Website not only helps to promote business but it is the front door of every organisation. It is <strong>NOT NECESSARY</strong> for every organisation to move towards <strong>Mobile Application</strong> but the website must be managed properly. <br /> <br />
+              Every website is gateway of information. Remembering my early days, there was no any source of information in web to access for checking results online but due to upgradation of technology, web, app and even from simple <strong>NOKIA</strong> phone we are able to check SEE (SLC) results online
             </p>
           </div>
 
