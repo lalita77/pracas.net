5c63443b3d796e3d5b81aeec9a21486c552c979d
pracas <p@sriyog.com>
2025-05-25 09:00:57 +0545

    Header text linked

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -5,7 +5,9 @@ function Header() {
     <>
       
       <div className="header bg-gray-100 sticky top-0 left-0 right-0 z-20">
-        <h1 className='flex place-content-center text-center text-[40px] font-bold text-[#4b4b4b] py-10'>PRACAS Blogs</h1>
+        <h1 className='flex place-content-center text-center text-[40px] font-bold text-[#4b4b4b] py-10'><a href="https://pracas.net">PRACAS Blogs</a></h1>
+         
+
        <hr />
         </div> 
     </>
