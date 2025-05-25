1f2453e6aa68f9264d1345ea6473ede2834f9e61
lalita77 <lalitaghimire330@gmail.com>
2025-05-25 16:00:24 +0545

    Update Content7.jsx

--- src/Details/Content7.jsx
+++ src/Details/Content7.jsx
@@ -20,7 +20,25 @@ function Content7() {
 
                 A business must be bounded by feelings, emotions and thoughts to change a society. We created thousands to billion dollars of empire, business, startups and a system where people can simply earn from few clicks in his/her smartphones. The use of technology has been in rapid growth stage that there are thousands of ways to earn, learn and lead the life easily. Technology has helped in many ways to get connected, either it is to become personal, professional or social. Thousands of Mobile Applications, different platforms, Gigabytes of storages and many megapixels of front and back camera. <br /> <br />
 
-                Airbnb started with a simple idea to share their homes, unused spaces, a airbed and hence billion dollar business. A true statement says that "Thousands of mile journey starts with a first step" where they themselves tried, where the idea was in mind, a simple thing, available resources and the simple website based on WordPress. Focused for International's Design Conference in 2007 Brain Chesky and his co-founder Joe Gebbia started renting their apartment with Air-Mattress which is now billion dollar business. The design conference which was held on October 2007 overcrowded city hotels so they say the opportunities to start something and solving something.
+                Airbnb started with a simple idea to share their homes, unused spaces, a airbed and hence billion dollar business. A true statement says that "Thousands of mile journey starts with a first step" where they themselves tried, where the idea was in mind, a simple thing, available resources and the simple website based on WordPress. Focused for <b>International's Design Conference in 2007</b> Brain Chesky and his co-founder Joe Gebbia started renting their apartment with Air-Mattress which is now billion dollar business. The design conference which was held on October 2007 overcrowded city hotels so they say the opportunities to start something and solving something.
+
+                <img className='rounded-2xl w-full max-h-[450px] lg:h-[450px] my-5 object-cover' src="/image/content7-image/2.jpeg" alt="" />
+
+                Airbnb founders Joe Gebbia, Nathan Blecharczyk, and Brian Chesky. : Source Airbnb <br />
+                <br /> 
+                The company now has moved very ahead than HomeAway, VRBO etc. Crossing different and difficult turns, now it has kept its name on the top of home sharing business. The early stage was difficult where they e-mailed design blog and conference organizers asking them to help promoting their website which they supported. <br /> <br />
+
+                It was hard to let a strangers staying and allocating own room but the things went smooth where people had to trust only by seeing profile, some registered and recorded information. <b>Technology helps to build better technology</b>. So the Airbnb did to establish and implement new ideas, verification system, beautiful photography, easy to use system and more which they faced as obstacle in their success and making dream come true. Where they once had said that once people will read a book about Airbnb which we have been reading it. They made a list of companies whose cultures they wanted to emulate.
+
+                 <img className='rounded-2xl w-full max-h-[450px] lg:h-[450px] my-5 object-cover' src="/image/content7-image/3.jpeg" alt="" />
+
+                 2013 was the year when Airbnb introduced ID Verification system which enhanced identity proofs and included more proof of identity where both hosts and guests have the options to only do business with verified id. The team of data scientists, who created behavioral models to help identify, used machine learning to develop tools that analyze reservations to help detect risk.
+
+                 <h5 className='font-semibold my-5'>"Early to bed, early to rise, and work like hell and organize" - Chris Lehane</h5>
+
+                 <img className='rounded-2xl w-full max-h-[450px] lg:h-[450px] my-5 object-cover' src="/image/content7-image/4.jpeg" alt="" />
+
+                Paris, Barcelona, and Santa Monica, Ca. have some of the strictest policies regarding who can and can't rent through Airbnb, while Amsterdam, Berlin, London, San Francisco, and New York have looser requirements. Though a billion dollar company, trusted by million people, had been growing like rocket, Airbnb is banned in many cities, areas and apartments. Apart form location ban, Airbnb has a process where they bans their host account by reviewing them. The suspension of account is managed & monitered according to the standards like how they have been maintaining. It also bans if a host has multiple accounts, ratings by guests and infringed terms of service. <br />
               </p>
             </div>
           </div>
